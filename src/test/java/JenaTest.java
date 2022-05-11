e1c3cbd4545916831549976ad1a06d86eb2caaa7
Maxime Jakubowski <maxime.jak@gmail.com>
2022-05-11 16:30:42 +0200

    added emptyjoin tests

--- src/test/java/JenaTest.java
+++ src/test/java/JenaTest.java
@@ -1,9 +1,13 @@
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.graph.Triple;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.rdfxml.xmloutput.impl.Basic;
 import org.apache.jena.sparql.algebra.Algebra;
 import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.algebra.op.*;
+import org.apache.jena.sparql.core.BasicPattern;
 import org.apache.jena.sparql.core.Var;
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +33,16 @@ public class JenaTest {
         Node lit = NodeFactory.createLiteral("lit");
 
         assert ! lit.toString().equals(lit.getLiteral().toString());
+    }
+
+    @Test
+    void emptyOpJoin() {
+        Op one = OpNull.create();
+        BasicPattern bp = new BasicPattern();
+        bp.add(new Triple(
+                NodeFactory.createURI("http://www.ex.com/a"),
+                NodeFactory.createURI("http://www.ex.com/a"),
+                NodeFactory.createURI("http://www.ex.com/a")));
+        Op two = new OpBGP(bp);
     }
 }
