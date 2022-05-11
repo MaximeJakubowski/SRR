6e306a6d75722a11a03f0770b9fd2fa8338b821a
Maxime Jakubowski <maxime.jak@gmail.com>
2022-05-11 16:33:13 +0200

    migrated to jena renaming api

--- src/test/java/RenamingTest.java
+++ src/test/java/RenamingTest.java
@@ -2,6 +2,8 @@ import org.apache.jena.query.QueryFactory;
 import org.apache.jena.sparql.algebra.Algebra;
 import org.apache.jena.sparql.algebra.Op;
 import org.apache.jena.sparql.algebra.Transformer;
+import org.apache.jena.sparql.core.Var;
+import org.apache.jena.sparql.syntax.syntaxtransform.QueryTransformOps;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -11,6 +13,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RenamingTest {
 
+    private Op applyRenaming(Map<String, String> renaming, String query) {
+        Map<Var, Var> varrename = new HashMap<>();
+        for (String key: renaming.keySet())
+            varrename.put(Var.alloc(key), Var.alloc(renaming.get(key)));
+
+        return Algebra.compile(QueryTransformOps.transform(QueryFactory.create(query), varrename));
+    }
+
     @Test
     void renamingEmpty() {
         Map<String, String> renaming = new HashMap<>();
@@ -18,7 +28,7 @@ public class RenamingTest {
         String query = "SELECT (COUNT(?o) AS ?c) WHERE { ?s <p> ?o } GROUP BY ?s ";
         Op queryOp = Algebra.compile(QueryFactory.create(query));
 
-        Op rewrittenQuery = Transformer.transform(new VariableRenamer(renaming), queryOp);
+        Op rewrittenQuery = applyRenaming(renaming, query);
 
         assertEquals(rewrittenQuery, queryOp);
     }
@@ -31,7 +41,7 @@ public class RenamingTest {
         String query = "SELECT (COUNT(?o) AS ?c) WHERE { ?s <p> ?o } GROUP BY ?s ";
         Op queryOp = Algebra.compile(QueryFactory.create(query));
 
-        Op rewrittenQuery = Transformer.transform(new VariableRenamer(renaming), queryOp);
+        Op rewrittenQuery = applyRenaming(renaming, query);
 
         System.out.println(queryOp);
         System.out.println(rewrittenQuery);
@@ -42,12 +52,13 @@ public class RenamingTest {
         Map<String, String> renaming = new HashMap<>();
         renaming.put("c", "r_c");
 
-        String query = "SELECT ?c\n" +
-                "WHERE { ?x <name> ?c }\n" +
-                "ORDER BY ?c";
+        String query = """
+                SELECT ?c
+                WHERE { ?x <name> ?c }
+                ORDER BY ?c""";
         Op queryOp = Algebra.compile(QueryFactory.create(query));
 
-        Op rewrittenQuery = Transformer.transform(new VariableRenamer(renaming), queryOp);
+        Op rewrittenQuery = applyRenaming(renaming, query);
 
         System.out.println(queryOp);
         System.out.println(rewrittenQuery);
@@ -61,7 +72,7 @@ public class RenamingTest {
         String query = "SELECT ?c WHERE { ?g <a> <graph> GRAPH ?g { ?x <name> ?c } }";
         Op queryOp = Algebra.compile(QueryFactory.create(query));
 
-        Op rewrittenQuery = Transformer.transform(new VariableRenamer(renaming), queryOp);
+        Op rewrittenQuery = applyRenaming(renaming, query);
 
         System.out.println(queryOp);
         System.out.println(rewrittenQuery);
